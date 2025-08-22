4ff7c9170f9a193b60e3c6aec50fbe33a5d2031a
Md Tanvir Ahamed Shanto <112241372+mdtanvirahamedshanto@users.noreply.github.com>
2025-08-22 15:15:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -757,11 +757,11 @@ app.patch("/add_views/:id", async (req, res) => {
 // -----------------
 // Sample route
 app.get("/", (req, res) => {
-  res.send("✅ The bd server is running");
+  res.send("✅ The au server is running");
 });
 
 // -----------------
 // Start server
 app.listen(port, () => {
-  console.log(`🚀the bd Server is running on port ${port}`);
+  console.log(`🚀the au Server is running on port ${port}`);
 });
